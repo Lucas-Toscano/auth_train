7adaf8772a882c90154f3cfd7aa234475d63008d
Lucas-Toscano <lucas_toscano00@hotmail.com>
2021-01-28 15:29:50 -0300

    user-form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # get "about-us", to: "about#index", as: :about
   get "about-us" => "about#index", as: :about
+
+  get "sign_up" => "registration#new"
+
+  post "sign_up" => "registration#create"
   
   root to: "main#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
