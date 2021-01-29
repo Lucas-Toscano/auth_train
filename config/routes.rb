12ad40b931e3163d84014f209835680de0dc77c3
Lucas-Toscano <lucas_toscano00@hotmail.com>
2021-01-29 15:34:51 -0300

    logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   get "about-us" => "about#index", as: :about
 
   get "sign_up" => "registration#new"
-
   post "sign_up" => "registration#create"
 
+  get "sign_in" => "sessions#new"
+  post "sign_in" => "sessions#create"
+  
   delete "logout" => "sessions#destroy"
   
   root to: "main#index"
