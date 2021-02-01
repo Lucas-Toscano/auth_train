52330e9e43af1a4057eec6efd2f683a4ae568a69
Lucas-Toscano <lucas_toscano00@hotmail.com>
2021-02-01 16:57:59 -0300

    reset_passwords

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # get "about-us", to: "about#index", as: :about
   get "about-us" => "about#index", as: :about
 
+  get "password" => "password#edit", as: :edit_password
+  patch "password" => "password#update"
+
   get "sign_up" => "registration#new"
   post "sign_up" => "registration#create"
 
@@ -9,7 +12,14 @@ Rails.application.routes.draw do
   post "sign_in" => "sessions#create"
   
   delete "logout" => "sessions#destroy"
+
+  get "password/reset" => "passwords_resets#new"
+  post "password/reset" => "passwords_resets#create"
+
+  get "password/reset/edit" => "passwords_resets#edit"
+  patch "password/reset/edit" => "passwords_resets#update"
   
+
   root to: "main#index"
   
 end
